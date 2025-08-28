0a6323bfb9751e0cb7187fb24a255a8e1040c3b4
Phong Duong <phongtdng@gmail.com>
2025-08-29 01:45:55 +0200

    split test to 2 tests for individual cases of parse

--- tests/testthat/test-download_dataset.R
+++ tests/testthat/test-download_dataset.R
@@ -1,4 +1,4 @@
-test_that("download_dataset saves and optionally parses", {
+test_that("download_dataset saves file when parse = FALSE", {
   skip_if_offline()
   tmp <- tempfile(); dir.create(tmp)
 
@@ -8,8 +8,17 @@ test_that("download_dataset saves and optionally parses", {
   out_path <- download_dataset(file_url, save_to = tmp, parse = FALSE)
   expect_type(out_path, "character")       # should be a string
   expect_true(file.exists(out_path))
+})
+
+test_that("download_dataset parses file when parse = TRUE", {
+  skip_if_offline()
+  tmp <- tempfile()
+  dir.create(tmp)
+
+  file_url <- "https://datos.madrid.es/egob/catalogo/300594-1-registro-AIP.csv"
+
+  out_obj <- download_dataset(file_url, save_to = tmp, parse = TRUE)
 
   # parse
-  out_parsed <- download_dataset(file_url, save_to = tmp, parse = TRUE)
-  expect_true(is.list(out_parsed) || inherits(out_parsed, "tbl_df"))
+  expect_true(inherits(out_obj, "tbl_df"))
 })
